daf21c3921ca04587d4de424ea5dc5a5c5c17462
ikenfin <ikenfin@gmail.com>
2018-06-25 19:26:50 +0400

    oc2 -> ocmod.xml, oc3 -> ocmod.zip

--- pack.sh
+++ pack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-zip -r oc3_module_design_position_dnd.ocmod.zip install.xml
-zip -r oc2_module_design_position_dnd.ocmod.zip upload
+rm ./*.{zip,xml}
+
+cp oc2/install.xml ./oc2_module_design_position_dnd.ocmod.xml
+cd oc3 && zip -r ../oc3_module_design_position_dnd.ocmod.zip install.xml
